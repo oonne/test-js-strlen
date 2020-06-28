2a10c92458063fb5edcf8f3b6963fb3e323154c9
JAY <JAY@oonne.com>
2020-06-28 10:17:01 +0800

    使用encodeURI来判断字符长度

--- actuator/strlen-js.js
+++ actuator/strlen-js.js
@@ -1,14 +1,31 @@
-let strlen = (str) => {
+/* 备选方案：使用charCodeAt判断字符 */ 
+let strlen0 = (str) => {
   if (!str) return 0;
   let len = 0;
   for (var i=0; i<str.length; i++) { 
     let c = str.charCodeAt(i); 
     if ((c >= 0x0001 && c <= 0x007e) || (0xff60<=c && c<=0xff9f)) { 
       len++; 
-    } else { 
+    } else {
      len+=3; 
     } 
   } 
+  return len;
+}
+
+/* 当前方案：使用encodeURI判断长度 */ 
+let strlen = (str) => {
+  if (!str) return 0;
+  let len = 0;
+  let chars = str.split('');
+  chars.map(char=>{
+    let urichar = encodeURI(char);
+    if (urichar.length == 1) {
+      len++;
+    } else {
+      len += urichar.length / 3; 
+    }
+  })
   return len;
 }
 
